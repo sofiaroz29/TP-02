4066ed3b036766fe3ef156ccf3fd0a531845c517
sofiaroz29 <sofiarozenberg29@gmail.com>
2022-04-03 20:45:07 -0300

    TP 02 Completed

---
+++ Assets/Scripts/MayordeEdad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayordeEdad : MonoBehaviour
+{
+
+    public int edadUsuario;
+    public Text miTexto;
+
+    void Start()
+    {
+       if (edadUsuario >= 18)
+       {
+            miTexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+       }
+
+        else
+        {
+            miTexto.text = "No es mayor de edad";
+            Debug.Log("No es mayor de Edad");
+        }
+    }
+
+   
+}
